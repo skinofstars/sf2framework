c316002cd64a2866b4ca2dd4a3d6989c5499feda
Kevin Carmody <kevin@whiteoctober.co.uk>
2012-01-20 11:47:04 +0000

    end of part 1

---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->register();
